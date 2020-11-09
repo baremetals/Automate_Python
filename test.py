1236cf7c915c11bdee06a61b1658c691ed01b502
Daniel Asante <baremetals16@gmail.com>
2020-11-09 16:49:19 +0000

    Initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+# this is a test program 
+
+print('hello World')
+print('what is your name?')
+my_name = input()
+print('its good to meet you, ' + my_name)
+print('the length of your name is:')
+print(len(my_name))
+print('what is your age?')
+myAge = input()
+print('you will be ' + str(int(myAge) + 1) + ' in a year.')
